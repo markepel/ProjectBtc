a111ee5bd13edffe7ca3849aa1b28437b921e26d
Epel Mark <epelmark@gmail.com>
2018-01-25 15:31:32 +0200

    add python-telegram-bot with updates not webhook

--- core.py
+++ core.py
@@ -7,23 +7,32 @@ import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 TOKEN = config.TOKEN
 bot = telegram.Bot(token=TOKEN)
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
 def start(bot, update):
     reply_keyboard = [['Стратегии'], ['Обучение','О боте'], ['Настройки', 'Личный кабинет']]
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    bot.send_message(chat_id=update.message.chat_id, text="Start clicked. Follow start!", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+def info(bot, update):
+    reply_keyboard = [['Стратегии'], ['Обучение','О боте'], ['Настройки', 'Личный кабинет']]
+    bot.send_message(chat_id=update.message.chat_id, text="Info clicked. Enjoy info!", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+
 
 
 start_handler = CommandHandler('start', start)
+info_handler = CommandHandler("info", info)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(info_handler)
+
 
 
 updater.start_polling()
