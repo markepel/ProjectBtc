ee15ca38e7b7e73324b3ee7509ebd3dc570ccfe5
Epel Mark <epelmark@gmail.com>
2018-03-14 20:38:29 +0200

    debug

--- publishStrategyConvHandler.py
+++ publishStrategyConvHandler.py
@@ -57,7 +57,11 @@ def finish(bot, update):
   db = DBRepo()
   global strategy_state
   idsToPublish = db.get_active_subscribers_ids_for_strategy_by_name(strategy_state[update.message.chat_id].strategyName)[0]
+  print("Ids to publish -- ", idsToPublish)
+
   for id in idsToPublish:
+    print("Idsdddddddddd -- ", id)
+
     bot.send_photo(chat_id=id, photo=strategy_state[update.message.chat_id].photoId, caption = strategy_state[update.message.chat_id].text, reply_markup=ReplyKeyboardMarkup(reply_keyboard_main_menu, one_time_keyboard=True), parse_mode=telegram.ParseMode.HTML)
     time.sleep(0.03)
 
