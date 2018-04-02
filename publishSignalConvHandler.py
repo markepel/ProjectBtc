72a1ee288e3e4457b5dcfcc2b2a72a79a10b3333
Epel Mark <epelmark@gmail.com>
2018-04-02 11:43:47 +0300

    T2

--- publishSignalConvHandler.py
+++ publishSignalConvHandler.py
@@ -64,9 +64,8 @@ def finish(bot, update):
     logger.info('signal_state - {0} for after finish in publishSignal for chat_id {1}'.format(signal_state, update.message.chat_id))
   except:
     logger.error("EEEEEEEEEEEEEE")
-    logger.info("IIIIIIIIIIIIII")
-    e = traceback.format_exc()
     logger.error("An error occured on publishsignal signal_state  = - \n {0}".format(signal_state))  
+    e = traceback.format_exc()
     logger.error("Error itself = \n {0}".format(e).encode('utf-8'))
   finally:
     return ConversationHandler.END
